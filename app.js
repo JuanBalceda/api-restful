dd0e8a0f42cabb568c28fc94614621ea8303e350
Juan Balceda <juanbalceda@gmail.com>
2018-11-27 04:01:48 +0100

    CORS enabled

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ app
     .set('port', process.env.PORT)
     .use(express.urlencoded({ extended: false }))
     .use(express.json())
+    .use((req, res, next) => {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    })
     .use('/api', router);
 
 module.exports = app;
