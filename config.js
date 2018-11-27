6c4d59f9563ab79dd02042ab363fb026405bcfa1
Juan Balceda <juanbalceda@gmail.com>
2018-11-27 22:18:51 +0100

    Add mLab data for MongoDB

--- config.js
+++ config.js
@@ -3,4 +3,6 @@ process.env.PORT = process.env.PORT || 3000
 
 if(process.env.NODE_ENV === 'dev'){
     process.env.URL_DB = 'mongodb://edteam:123456@localhost:27017/marathon_app';
+}else{
+    process.env.URL_DB = 'mongodb://jbalceda:edteam123456@ds119024.mlab.com:19024/jb-marathons';
 }
